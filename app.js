6e2ddd17f29c9a4aaa69e09077010464b6a756bf
Karan Khandekar <karankhandekar028@gmail.com>
2026-05-22 19:54:50 +0530

    Add file upload feature; Show upload progress; Add file rename feature; Changed links to buttons

--- app.js
+++ app.js
@@ -1,68 +1,91 @@
 import { open, readdir } from "node:fs/promises";
 import http from "node:http";
 import mime from 'mime-types';
-import { ReadStream } from "node:fs";
+import { createWriteStream, ReadStream } from "node:fs";
+import fs from 'node:fs/promises'
 
 const server = http.createServer(async (req, res) => {
   res.setHeader("Access-Control-Allow-Origin", "*")
+  res.setHeader("Access-Control-Allow-Headers", "*")
+  res.setHeader("Access-Control-Allow-Methods", "*")
 
-  let [url, _query] = req.url.split("?"); 
-  url = decodeURIComponent(url);
-
-  if (url === "/favicon.ico") return res.end("No favicon")
-
-  const rootFolder = url === "/"
-  const rootUrl = `./storage${url}`;
-
-  const queryParams = {};
-  _query?.split("&").forEach((pair) => {
-    const [key, value] = pair.split("=")
-    queryParams[key] = value;
-  })
-
-  try {
-    const fileHandle = await open(rootUrl);
-    const stats = await fileHandle.stat();
-
-    if (stats.isDirectory()) {
-      // const path = url.split("/")
-      // const currentFolder = rootFolder ? "Storage" : path[path.length - 1];
-
-      const filesInDIR = await readdir(rootUrl);
-
-      
-      // let endRes = `<h1>Storage App</h1><h2>Current folder: ${currentFolder}</h2><h3>Files: </h3>`;
-      
-      // filesInDIR.forEach((file) => {
-      //   const link = (rootFolder ? "" : url + "/") + encodeURIComponent(file);
-      //   endRes += `${file} <a href=${link + "?action=open"}>Open</a>\t<a href=${link + "?action=download"}>Download</a><br>`;
-      // });
-      
-      res.setHeader("Content-Type", "application/json");
-      res.end(JSON.stringify(filesInDIR));
-    } else {
-      const mimeType = mime.lookup(rootUrl) || "application/octet-stream";
-      const open = queryParams?.action != "download";
-
-      res.setHeader("Content-Type", mimeType);
-      res.setHeader("Content-Length", stats.size);
-      res.setHeader("Content-Disposition", open ? "inline" : "attachment");
-
-      const readStream = fileHandle.createReadStream(rootUrl); // reads file data in chunks
-      readStream.pipe(res); // connects readable to writable stream;
-
-      // ^ This is basically this V
-      // readStream.on("data", (chunk) => {
-      //   res.write(chunk);
-      // });
-
-      // readStream.on("end", () => {
-      //   res.end();
-      // });
+  if (req.method === "GET") {
+    let [url, _query] = req.url.split("?"); 
+    url = decodeURIComponent(url);
+  
+    if (url === "/favicon.ico") return res.end("No favicon")
+  
+    const rootFolder = url === "/"
+    const rootUrl = `./storage${url}`;
+  
+    const queryParams = {};
+    _query?.split("&").forEach((pair) => {
+      const [key, value] = pair.split("=")
+      queryParams[key] = value;
+    })
+  
+    try {
+      const fileHandle = await open(rootUrl);
+      const stats = await fileHandle.stat();
+  
+      if (stats.isDirectory()) {
+        // const path = url.split("/")
+        // const currentFolder = rootFolder ? "Storage" : path[path.length - 1];
+  
+        const filesInDIR = await readdir(rootUrl);
+  
+        
+        // let endRes = `<h1>Storage App</h1><h2>Current folder: ${currentFolder}</h2><h3>Files: </h3>`;
+        
+        // filesInDIR.forEach((file) => {
+        //   const link = (rootFolder ? "" : url + "/") + encodeURIComponent(file);
+        //   endRes += `${file} <a href=${link + "?action=open"}>Open</a>\t<a href=${link + "?action=download"}>Download</a><br>`;
+        // });
+        
+        res.setHeader("Content-Type", "application/json");
+        res.end(JSON.stringify(filesInDIR));
+      } else {
+        const mimeType = mime.lookup(rootUrl) || "application/octet-stream";
+        const open = queryParams?.action != "download";
+  
+        res.setHeader("Content-Type", mimeType);
+        res.setHeader("Content-Length", stats.size);
+        res.setHeader("Content-Disposition", open ? "inline" : "attachment");
+  
+        const readStream = fileHandle.createReadStream(rootUrl); // reads file data in chunks
+        readStream.pipe(res); // connects readable to writable stream;
+  
+        // ^ This is basically this V
+        // readStream.on("data", (chunk) => {
+        //   res.write(chunk);
+        // });
+  
+        // readStream.on("end", () => {
+        //   res.end();
+        // });
+      }
+    } catch (error) {
+      console.error(error.message)
+      res.end(JSON.stringify("Not found"))
     }
-  } catch (error) {
-    console.error(error.message)
-    res.end(JSON.stringify("Not found"))
+  } else if (req.method === "OPTIONS") {
+    res.end("OK")
+  } else if (req.method === "POST") {
+      const writeStream = createWriteStream(`./storage/${req.headers.filename}`)
+      let count = 0
+      req.on("data", (chunk) => {
+        console.log(count++)
+        writeStream.write(chunk)
+      })
+      req.on('end', () => {
+        res.end("file uploaded to server")
+      })
+  } else if (req.method === "PATCH") {
+    const originalName = `./storage/${req.headers.originalname}`
+    const fileExtension = originalName.split(".").pop()
+    const newName = `./storage/${req.headers.newfilename}.${fileExtension}`
+    await fs.rename(originalName, newName)
+    res.end("File name changed")
   }
 })
 
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,30 +4,82 @@ import { useEffect } from "react"
 
 export default function App() {
   const [filesInDIR, setFilesInDIR] = useState([]);
+  const [progress, setProgress] = useState('')
+  const [renamingFile, setRenamingFile] = useState(null)
+  const [renameText, setRenameText] = useState('')
+
+  const fetchFiles = async () => {
+      const res = await fetch("http://localhost:4000/")
+      const data = await res.json()
+      console.log(data)
+      setFilesInDIR(data)
+  }
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const res = await fetch("http://localhost:4000/")
-        const data = await res.json()
-        console.log(data)
-        setFilesInDIR(data)
-      } catch (error) {
-        console.error(error.message)
-      }
-    }
-
-    fetchData()
+    fetchFiles()
   }, [])
+
+  const handleOnChange = (e) => {
+    const uploadedFile = e.target.files[0]
+    if (!uploadedFile) return;
+      const xhr = new XMLHttpRequest();
+      xhr.open("POST", "http://localhost:4000/", true);
+      xhr.setRequestHeader("filename", uploadedFile.name)
+      xhr.onload = () => {
+        console.log(xhr.responseText)
+      };
+      xhr.upload.onprogress = (e) => {
+        const progress = ((e.loaded / e.total) * 100)
+        setProgress(`${progress.toFixed(2)}%`)
+      }
+      xhr.onerror = () => {
+        console.log("Upload failed")
+      }
+
+      xhr.send(uploadedFile);
+      fetchFiles()
+      setTimeout(() => {
+        setProgress('')
+      }, 2000)
+  }
+
+  const handleRename = async (originalName) => {
+    const response = await fetch('http://localhost:4000/', {
+      method: 'PATCH',
+      headers: {
+        "originalname": `${originalName}`,
+        "newfilename": `${renameText}`
+      }
+    })
+
+    const data = await response.text()
+    console.log(data)
+    setRenameText('')
+    setRenamingFile(null)
+    fetchFiles()
+  }
 
   return (
     <>
       <h1>My Files</h1>
         {filesInDIR.map((file, index) => (    
           <div key={index}>
-            {file} <a href={`http://localhost:4000/${file}?action=open`}>Open</a>{" "}<a href={`http://localhost:4000/${file}?action=download`}>Download</a>
+            {file}
+            <button onClick={() => window.open(`http://localhost:4000/${file}?action=open`)}>Open</button>
+            <button onClick={() => window.open(`http://localhost:4000/${file}?action=download`)}>Download</button>
+            <button onClick={() => setRenamingFile(file)}>Rename</button>
+            { renamingFile === file &&
+              <>
+                <input type="text" value={renameText} onChange={(e) => (setRenameText(e.target.value))} /> 
+                <button onClick={() => handleRename(file)}>Save</button> 
+              </>
+            }
           </div>
         ))}
+
+        <input type="file" onChange={(e) => handleOnChange(e)}/>
+        {progress && <p>Progress: {progress}</p>}
+
     </>
   )
 }
